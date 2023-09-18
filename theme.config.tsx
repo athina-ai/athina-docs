67ed2a32329f367260958ccf923df9dd50ba76e6
Shiv Sakhuja <shivsak.s@gmail.com>
2023-09-17 19:04:31 -0700

    Initial commit: Added skeleton and filled in most pages with docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <div style={{ display: "flex" }}>
+      <img
+        src="/magik-logo.png"
+        style={{
+          width: "28px",
+          height: "28px",
+          borderRadius: "4px",
+          marginRight: "10px",
+        }}
+      />
+      Magik
+    </div>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra Docs Template",
   },
-}
+};
 
-export default config
+export default config;
